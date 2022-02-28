48c77413c266abf3a362f361a4b98ed611aea8de
markusribe <89196256+markusribe@users.noreply.github.com>
2022-02-28 09:36:35 +0100

    Update quote.go

--- myquote/quote.go
+++ myquote/quote.go
@@ -1,5 +1,21 @@
-package main
+package myquote
 
-func main() {
-	$END$
+import (
+	"rsc.io/quote"
+)
+
+func Glass() string {
+	return quote.Glass()
+}
+
+func Go() string {
+	return quote.Go()
+}
+
+func Hello() string {
+	return quote.Hello()
+}
+
+func Opt() string {
+	return quote.Opt()
 }
